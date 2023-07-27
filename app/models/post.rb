4519683f06264ced5ab88eb9c01e37c213180bd5
KotaMurasugi <132252846+KotaMurasugi@users.noreply.github.com>
2023-07-27 03:20:04 +0000

    add favorite function

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,14 @@
 class Post < ApplicationRecord
   has_many_attached :images
   belongs_to :user
-  has_many   :comments, dependent: :destroy
+  has_many   :comments,  dependent: :destroy
+  has_many   :favorites, dependent: :destroy
 
+  #バリデーション
+  validates :title, presence: true
+  validates :body,  presence: true
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
 
     get 'homes/about' => 'homes#about', as: 'about'
     resources :posts, only: [:new, :create, :index, :show, :destroy] do
-      resources :comments, only: [:create, :destroy]
+      resource  :favorites, only: [:create, :destroy]
+      resources :comments,  only: [:create, :destroy]
     end
     resources :users, only: [:show, :index, :edit, :update]
   end
