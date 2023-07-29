69f73092451e6ba444081f1057d31ce04bed2ed1
KotaMurasugi <132252846+KotaMurasugi@users.noreply.github.com>
2023-07-29 12:40:06 +0000

    2023/07/29 21:40

--- db/migrate/20230729115450_change_taggings_post_id_to_bigint.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTaggingsPostIdToBigint < ActiveRecord::Migration[6.1]
-  def change
-    change_column :taggings, :post_id, :bigint
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_29_115450) do
+ActiveRecord::Schema.define(version: 2023_07_27_150842) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -76,7 +76,7 @@ ActiveRecord::Schema.define(version: 2023_07_29_115450) do
   end
 
   create_table "taggings", force: :cascade do |t|
-    t.bigint "post_id", null: false
+    t.integer "post_id", null: false
     t.integer "tag_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(version: 2023_07_29_115450) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "taggings", "posts"
   add_foreign_key "taggings", "posts"
   add_foreign_key "taggings", "tags"
 end
