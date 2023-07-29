9bc977833775e94d50173af634dbf3bdcee48f09
KotaMurasugi <132252846+KotaMurasugi@users.noreply.github.com>
2023-07-29 13:00:52 +0000

    2023/07/29 22:00

--- db/migrate/20230727150553_create_taggings.rb
+++ db/migrate/20230727150553_create_taggings.rb
@@ -2,8 +2,8 @@ class CreateTaggings < ActiveRecord::Migration[6.1]
   def change
     create_table :taggings do |t|
 
-      t.references :post, null: false, foreign_key: true
-      t.references :tag,  null: false, foreign_key: true
+      t.integer :post_id, null: false, foreign_key: true
+      t.integer :tag_id,  null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -80,8 +80,6 @@ ActiveRecord::Schema.define(version: 2023_07_29_125057) do
     t.integer "tag_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_taggings_on_post_id"
-    t.index ["tag_id"], name: "index_taggings_on_tag_id"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -107,8 +105,4 @@ ActiveRecord::Schema.define(version: 2023_07_29_125057) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "taggings", "posts"
-  add_foreign_key "taggings", "posts"
-  add_foreign_key "taggings", "posts"
-  add_foreign_key "taggings", "tags"
 end
